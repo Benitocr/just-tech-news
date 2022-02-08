d272c9bd9a052eba9ceadb31106b04edb3fba97f
Benito <benitocavazosr@yahoo.com>
2022-02-08 01:48:18 -0600

    adding votes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -22,19 +22,32 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
           id: req.params.id
-        }
-      })
-        .then(dbUserData => {
-          if (!dbUserData) {
+        },
+        include: [
+            {
+              model: Post,
+              attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+              model: Post,
+              attributes: ['title'],
+              through: Vote,
+              as: 'voted_posts'
+            }
+        ]
+
+    })
+    .then(dbUserData => {
+            if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' });
             return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 
 
 });
