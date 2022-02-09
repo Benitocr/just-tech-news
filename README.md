998d2c074e77508afd380f48f89095c027d72937
Benito <benitocavazosr@yahoo.com>
2022-02-09 00:13:37 -0600

    adding comments to user and posts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # just-tech-news
+
+<!-- ME quede en 13.5.5 -->
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote} = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get('/', (req, res) => {
@@ -17,6 +17,15 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']], 
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                }
+            },
+
             {
             model: User,
             attributes: ['username']
@@ -43,11 +52,20 @@ router.get('/:id', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
+        order: [['created_at', 'DESC']], 
         include: [
-        {
-            model: User,
-            attributes: ['username']
-        }
+          {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+          }, 
+          {
+              model: User,
+              attributes: ['username']
+          }
         ]
     })
     .then(dbPostData => {
